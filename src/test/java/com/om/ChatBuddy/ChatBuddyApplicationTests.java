f44e6412a214f51f4cedc419115f468107672fbf
vaibhav <vaibhav.bhadane@ilmtec.in>
2026-05-09 07:33:44 +0530

    Initial Spring Boot commit

---
+++ src/test/java/com/om/ChatBuddy/ChatBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om.ChatBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
