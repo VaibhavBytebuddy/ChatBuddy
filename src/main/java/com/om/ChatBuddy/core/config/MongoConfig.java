bc5e7c51263ed7dda815a1fcbbb362e35ff962e7
vaibhav <vaibhav.bhadane@ilmtec.in>
2026-05-09 08:23:27 +0530

    Infrastructure: Add MongoConfig for auditing enablement

---
+++ src/main/java/com/om/ChatBuddy/core/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.om.ChatBuddy.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
